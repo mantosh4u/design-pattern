2d83aa122a291020300d637406815f0bdab736d9
mantosh4u <mantosh4u@gmail.com>
2014-11-05 07:25:56 +0530

    Revert "Used standard algorithms and move semantics where appropriate"

--- GOF-Patterns/List.hpp
+++ GOF-Patterns/List.hpp
@@ -1,51 +1,73 @@
-#include <list>
-#include <exception>
-#include <iostream>
-#include <algorithm>
-#include <utility>
-#include <iterator>
+#include<list>
+#include<exception>
+#include<iostream>
+#include<algorithm>
+#include<utility>
+
+
 
 /* Wrapper class List based on STL std::list */
-template <typename Item>
+template<typename Item>
 class List {
 public:
     void Append(Item obj);
     void Remove(Item obj);
-    void display();
+    void display(void);
     int  Count() const;
     Item Get(int index) const;
 private:
     std::list<Item> _collection;
 };
 
-template <typename Item>
+
+
+template<typename Item>
 void List<Item>::Append(Item obj) {
-    _collection.push_back(std::move(obj));
+    _collection.push_back(obj);
 }
 
-template <typename Item>
+
+
+template<typename Item>
 void List<Item>::Remove(Item obj) {
-    _collection.erase(std::find(std::begin(_collection), std::end(_collection), std::move(obj)));
+    auto pos = std::find(std::begin(_collection), std::end(_collection), obj);
+    if (pos != std::end(_collection)) {
+        _collection.erase(pos);
+    }
 }
 
-template <typename Item>
-void List<Item>::display() {
-    std::copy(std::begin(_collection), std::end(_collection), std::ostream_iterator<Item>(std::cout, "\t"));
+
+
+template<typename Item>
+void List<Item>::display(void) {
+    for (const auto& itr : _collection) {
+        std::cout << itr << "\t";
+    }
     std::cout << std::endl;
 }
+
+
 
 template<typename Item>
 int List<Item>::Count() const {
     _collection.size();
 }
 
-template <typename Item>
+
+
+template<typename Item>
 Item List<Item>::Get(int index) const {
-    if (index >= _collection.size())
-        throw std::out_of_range("Out Of Range Error");
-    
-    return std::next(std::begin(_collection), index);
+    if (index < _collection.size()) {
+        auto iter = _collection.begin();
+        while (index--) {
+            ++iter;
+        }
+        return *iter;
+    }
+    throw std::out_of_range("Out Of Range Error");
 }
+
+
 
 /* Iterator base class to get the items from the collection like List */
 template <class Item>
@@ -54,11 +76,13 @@ public:
     virtual void First() = 0;
     virtual void Next() = 0;
     virtual bool IsDone()= 0;
-    virtual Item CurrentItem() = 0;
-    virtual ~Iterator() = default;
+    virtual Item CurrentItem()= 0;
 protected:
-    Iterator() = default;
+    Iterator(){
+    }
 };
+
+
 
 /* Iterator concrete class to traverse the List container class */
 template <typename Item>
@@ -74,27 +98,32 @@ private:
     int _current;
 };
 
-template <typename Item>
-ListIterator<Item>::ListIterator(const List<Item>* aList) : _list(aList), _current(0) { }
 
-template <typename Item>
+template<typename Item>
+ListIterator<Item>::ListIterator(const List<Item>* aList) :
+_list(aList), _current(0) {
+}
+
+template<typename Item>
 void ListIterator<Item>::First() {
     _current = 0;
 }
 
-template <typename Item>
+template<typename Item>
 void ListIterator<Item>::Next() {
-    std::advance(_current, 1);
+    _current++;
 }
 
-template <typename Item>
+template<typename Item>
 bool ListIterator<Item>::IsDone() {
     return (_current >= _list->Count());
 }
 
-template <typename Item>
+template<typename Item>
 Item ListIterator<Item>::CurrentItem() {
-    if (IsDone())
+    if (IsDone()) {
         throw std::out_of_range("Out Of Range Error");
+    }
     return _list->Get(_current);
 }
+
