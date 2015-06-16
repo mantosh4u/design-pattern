0d4734cd5a5f4cde387072521a3cabfac78c3610
Mantosh Kumar <mantosh4u@gmail.com>
2015-06-16 23:40:05 +0530

    Create client.cpp

---
+++ gof_creational_pattern/client.cpp
@@ -0,0 +1,19 @@
+#include"creational_conventional.h"
+#include"abstract_factory.h"
+#include"factory_method.h"
+#include"Prototype.h"
+#include"Builder.h"
+
+
+
+int main() 
+{
+	// creational_conventional::creational_conventional_approach();
+	// abstract_factory::creational_abstract_factory_approach();
+	// factory_method::creational_factory_method_approach();
+	// prototype::creational_prototype_approach();
+	
+	builder::creational_builder_approach();
+	
+	return 0;
+}
